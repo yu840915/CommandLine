bd3875c988081bd21d3532fd2ecedc75752dcbd7
yu840915 <mike@17.media>
2017-05-09 15:08:47 +0800

    Add static lib target

---
+++ CommandLineArgumentParser/CommandLineArgumentParser.h
@@ -0,0 +1,13 @@
+//
+//  CommandLineArgumentParser.h
+//  CommandLineArgumentParser
+//
+//  Created by Mike Yu on 09/05/2017.
+//  Copyright © 2017 Ben Gollmer. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface CommandLineArgumentParser : NSObject
+
+@end
---
+++ CommandLineArgumentParser/CommandLineArgumentParser.m
@@ -0,0 +1,13 @@
+//
+//  CommandLineArgumentParser.m
+//  CommandLineArgumentParser
+//
+//  Created by Mike Yu on 09/05/2017.
+//  Copyright © 2017 Ben Gollmer. All rights reserved.
+//
+
+#import "CommandLineArgumentParser.h"
+
+@implementation CommandLineArgumentParser
+
+@end
